2b1a3063ec9fb6d355918cadf5fc4b5de78671a1
Lucas Suzuki <lucas.suzuki2104@gmail.com>
2026-03-13 21:16:22 -0300

    adid html e funcionalidade

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from flask import Flask
 
+
 app = Flask(__name__)
 
-@app.route("/")
-def homepage():
-    return "<h1>Hello World!</h1>"
+from views import *
+
+
 
 if __name__ == "__main__":
     app.run()
---
+++ views.py
@@ -0,0 +1,23 @@
+from os import name
+
+from main import app
+from flask import render_template, request, redirect, url_for
+
+lista_ratings = []
+@app.route("/")
+def homepage():
+    return render_template("homepage.html")
+
+@app.route("/enviar", methods=["POST"])
+def enviar():
+    nome = request.form["nome"]
+    nota = request.form["nota"]
+
+
+    lista_ratings.append({"nome": nome, "nota": nota})
+
+    return redirect(url_for("ratings"))
+@app.route("/ratings")
+def ratings():
+    return render_template("ratingPage.html", ratings=lista_ratings)
+
